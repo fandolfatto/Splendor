4c1068e67c8fd3c379d5858da1ba6966256bf8b9
Frédérique Andolfatto <frederique.andolfatto@cpnv.ch>
2018-08-28 21:39:20 +0200

    Première version pour les élèves

---
+++ Splendor/ConnectionDB.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Splendor
+{
+    /// <summary>
+    /// contains methods and attributes to connect and deal with the database
+    /// TO DO : le modèle de données n'est pas super, à revoir!!!!
+    /// </summary>
+    class ConnectionDB
+    {
+        //connection to the database
+        private SQLiteConnection m_dbConnection; 
+
+        /// <summary>
+        /// constructor : creates the connection to the database SQLite
+        /// </summary>
+        public ConnectionDB()
+        {
+
+            SQLiteConnection.CreateFile("Splendor.sqlite");
+            
+            m_dbConnection = new SQLiteConnection("Data Source=Splendor.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            CreateInsertPlayer();
+            
+        }
+
+        /// <summary>
+        /// create the table "player" and insert data
+        /// </summary>
+        private void CreateInsertPlayer()
+        {
+            string sql = "CREATE TABLE player (id INT, pseudo VARCHAR(20))";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into player (id, pseudo) values (0, 'Fred')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into player (id, pseudo) values (1, 'Harry')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into player (id, pseudo) values (2, 'Sam')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        
+        /// <summary>
+        /// get the name of the player according to his id
+        /// </summary>
+        /// <param name="id">id of the player</param>
+        /// <returns></returns>
+        public string GetPlayerName(int id)
+        {
+            string sql = "select pseudo from player where id = " + id;
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            string name = "";
+            while (reader.Read())
+            {
+                name = reader["pseudo"].ToString();
+            }
+            return name;
+        }
+        
+    }
+}
---
+++ Splendor/Form1.cs
@@ -0,0 +1,260 @@
+﻿/**
+ * \file      frmAddVideoGames.cs
+ * \author    F. Andolfatto
+ * \version   1.0
+ * \date      August 22. 2018
+ * \brief     Form to play.
+ *
+ * \details   This form enables to choose coins or cards to get ressources (precious stones) and prestige points 
+ * to add and to play with other players
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Splendor
+{
+    /// <summary>
+    /// manages the form that enables to play with the Splendor
+    /// </summary>
+    public partial class frmSplendor : Form
+    {
+        //used to store the number of coins selected for the current round of game
+        private int nbRubis;
+        private int nbOnyx;
+        private int nbEmeraude;
+        private int nbDiamand;
+        private int nbSaphir;
+
+        //id of the player that is playing
+        private int currentPlayerId;
+        //boolean to enable us to know if the user can clic on a coin or a card
+        private bool enableClicLabel;
+        //connection to the database
+        private ConnectionDB conn;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public frmSplendor()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// loads the form and initialize data in it
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmSplendor_Load(object sender, EventArgs e)
+        {
+            lblGoldCoin.Text = "5";
+
+            lblDiamandCoin.Text = "7";
+            lblEmeraudeCoin.Text = "7" ;
+            lblOnyxCoin.Text = "7";
+            lblRubisCoin.Text = "7";
+            lblSaphirCoin.Text = "7";
+
+            Card card11 = new Card();
+            card11.Level = 1;
+            card11.PrestigePt = 1;
+            card11.Cout = new int[] { 1, 0, 2, 0, 2 };
+
+            Card card12 = new Card();
+            card12.Level = 1;
+            card12.PrestigePt = 0;
+            card12.Cout = new int[] { 0, 1, 2, 1, 0 };
+
+            txtLevel11.Text = card11.ToString();
+            txtLevel12.Text = card12.ToString();
+
+            conn = new ConnectionDB();
+
+            //load cards from the database
+            //TO DO
+
+            this.Width = 680;
+            this.Height = 540;
+
+            enableClicLabel = false;
+
+            lblChoiceDiamand.Visible = false;
+            lblChoiceOnyx.Visible = false;
+            lblChoiceRubis.Visible = false;
+            lblChoiceSaphir.Visible = false;
+            lblChoiceEmeraude.Visible = false;
+            cmdValidateChoice.Visible = false;
+            cmdNextPlayer.Visible = false;
+
+        }
+
+        /// <summary>
+        /// clic on the play button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmdPlay_Click(object sender, EventArgs e)
+        {
+            this.Width = 680;
+            this.Height = 780;
+
+            int id = 0;
+           
+            LoadPlayer(id);
+
+        }
+
+
+        /// <summary>
+        /// load data about the current player
+        /// </summary>
+        /// <param name="id">identifier of the player</param>
+        private void LoadPlayer(int id) { 
+
+            enableClicLabel = true;
+
+            string name = conn.GetPlayerName(currentPlayerId);
+
+            //no coins or card selected yet, labels are empty
+            lblChoiceDiamand.Text = "";
+            lblChoiceOnyx.Text = "";
+            lblChoiceRubis.Text = "";
+            lblChoiceSaphir.Text = "";
+            lblChoiceEmeraude.Text = "";
+
+            lblChoiceCard.Text = "";
+
+            //no coins selected
+            nbDiamand = 0;
+            nbOnyx = 0;
+            nbRubis = 0;
+            nbSaphir = 0;
+            nbEmeraude = 0;
+
+            Player player = new Player();
+            player.Name = name;
+            player.Id = id;
+            player.Ressources = new int[] { 2, 0, 1, 1, 1 };
+            player.Coins = new int[] { 0, 1, 0, 1, 1 };
+
+            lblPlayerDiamandCoin.Text = player.Coins[0].ToString();
+            lblPlayerOnyxCoin.Text = player.Coins[1].ToString();
+            lblPlayerRubisCoin.Text = player.Coins[2].ToString();
+            lblPlayerSaphirCoin.Text = player.Coins[3].ToString();
+            lblPlayerEmeraudeCoin.Text = player.Coins[4].ToString();
+            currentPlayerId = id;
+
+            lblPlayer.Text = "Jeu de " + name;
+
+            cmdPlay.Enabled = false;
+        }
+
+        /// <summary>
+        /// clic on the red coin (rubis) to tell the player has selected this coin
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblRubisCoin_Click(object sender, EventArgs e)
+        {
+            if (enableClicLabel)
+            {
+                cmdValidateChoice.Visible = true;
+                lblChoiceRubis.Visible = true;
+                //TO DO check if possible, update the number of available coin
+                nbRubis++;
+                lblChoiceRubis.Text = nbRubis + "\r\n";
+            }
+        }
+
+        /// <summary>
+        /// clic on the blue coin (saphir) to tell the player has selected this coin
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblSaphirCoin_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// clic on the black coin (onyx) to tell the player has selected this coin
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblOnyxCoin_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// clic on the green coin (emeraude) to tell the player has selected this coin
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblEmeraudeCoin_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        /// <summary>
+        /// clic on the white coin (diamand) to tell the player has selected this coin
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblDiamandCoin_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// clci on the validate button to approve the selection of coins or card
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmdValidateChoice_Click(object sender, EventArgs e)
+        {
+            cmdNextPlayer.Visible = true;
+            //TO DO Check id card or coins selected, impossible to do both at the same time
+
+            
+            
+            cmdNextPlayer.Enabled = true;
+        }
+
+        /// <summary>
+        /// clic on the insert button to insert player in the game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmdInsertPlayer_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A implémenter");
+        }
+
+        /// <summary>
+        /// clic on the next player to tell him it is his turn
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmdNextPlayer_Click(object sender, EventArgs e)
+        {
+            //TO DO in release 1.0 : 3 is hard coded (number of players for the game), it shouldn't. 
+            currentPlayerId = (currentPlayerId + 1 ) % 3;
+            LoadPlayer(currentPlayerId);
+
+            cmdNextPlayer.Enabled = false;
+            
+        }
+
+    }
+}
